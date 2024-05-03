40f74534eac8fb504cae3f1753bd3d06d5a9d93a
Bryan Hoffmaster <bryan.hoffmaster@gmail.com>
2024-05-03 12:17:16 -0500

    Remove jQuery, html input, and use (mostly) plain text as the input * Added a bunch of async/await/promises to handle "typing" animations MUCH more smoothly * Still have a lot of little styling left to go * added a list of final TODOs * added async file loading * moved away from name object literal to class object to better handle lexical scope with anon func auto closure * named things way more descriptively than inspiration setup was done * added test file Links.txt for link testing/typing * all links will now open in new tab (wanted to force middle click but apparently that's just not possible). * typing timings are still set for debugging

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-// TODO:
-// * See if you need to do a "defer" before running the script or set it up to run at the end of the page.
-// * Interactive CLI options via input element 
-//    * build listeners to update the DOM and react to input element
-//    * teardown listeners?
-//    * Always pre-face input with CLI prompt
-// * Read in document(s) and append content 
-//    * Content should be displayed in the same order as the document
-//    * Empty spaces are new paragraphs (update css p tags to accommodate)
-//    * Apply the correct tags and classes to each paragraph and content
+// TODO: Any click anywhere should put focus on the cli_input element!
+// TODO: have a cool animation "flash" for each char "typed" out outside of the cli_input per row ---->
+// TODO: Set font-family
+// TODO: how to handle generating inline links in text? Maybe just avoid them all together for this project...
+// TODO: set layout styles for the display and content elements
+// TODO: set style and coloring for the cli_input element
+// TODO: test various colors for your prompt and display elements
+// TODO: use emojis in your text?? Like add a fire emoji to skills that I'm great at.
+// TODO: "Headers" with markdown type formatting?
+// TODO: "Centered text" - will need a another line prefix, maybe.
 
 // NOTE: Inspired to do this from here: https://github.com/CodeNerve/CodeNerve.github.io/tree/master
 
@@ -26,113 +26,178 @@ var getFileText = async (file) => {
     }
 }
 
-// TODO: Clean this up and make more modular, this is the OG JS code you copied from.
-var CLI_Display = {
-    text: '',
-    accessCountTimer: null,
-    index: 0,
-    speed: 2,
-    file: '',
-    accessCount: 0,
-    deniedCount: 0,
+class CLI_Display {
+    cli_input = null;
+    prompt = null;
+    prompt_wrapper = null;
+    display = null;
+    content = null;
+    currentParagraph = null;
 
-    init: async () => {
-        const fileText = await getFileText('BryanHoffmaster.txt');
-        this.CLI_Display.text = fileText ?? '';
-        this.CLI_Display.text = CLI_Display.text.slice(0, CLI_Display.text.length - 1);
-        this.CLI_Display.updateLastCharacter();
-    },
-    // init: function () {
-    //     accessCountTimer = setInterval(function () {
-    //         CLI_Input.updateLastCharacter();
-    //     }, 500);
-    //     $.get(CLI_Input.file, function (data) {
-    //         CLI_Input.text = data;
-    //         CLI_Input.text = CLI_Input.text.slice(0, CLI_Input.text.length - 1);
-    //     });
-    // },
+    text = '';
+    textLines = [];
+    fileName = '';
+    cursorBlinkRef = null;
+    typingTimeout = null;
 
-    // TODO: 
-    content: ()=> {
-        var content = document.getElementById('content');
-        return content;
-    },
-    console: ()=> {
-        var console = document.getElementById('console');
-        return console;
-    },
-
-    display: ()=> {
-        var display = document.getElementById('display');
-        return display;
-    },
-
-    write: (str = '')=> {
-        var cont = this.CLI_Display.content();
-        cont.innerHTML = cont.append(str);
-    },
-
-    addText: function (key) {
-        
-        if (this.CLI_Display.text) {
-            var cont = CLI_Display.content();
-            if (cont.substring(cont.length - 1, cont.length) == '|')
-                $('#console').html(
-                    $('#console')
-                        .html()
-                        .substring(0, cont.length - 1),
-                );
-            if (key.keyCode != 8) {
-                CLI_Display.index += CLI_Display.speed;
-            } else {
-                if (CLI_Display.index > 0) CLI_Display.index -= CLI_Display.speed;
-            }
-            var text = CLI_Display.text.substring(0, CLI_Display.index);
-            var rtn = new RegExp('\n', 'g');
-
-            $('#console').html(text.replace(rtn, '<br/>'));
-            window.scrollBy(0, 50);
+    constructor(entryFileName = '') {
+        this.fileName = entryFileName;
+        if (this.fileName) {
+            this.init();
         }
-    },
+    }
 
-    getCLIPrompt: ()=> {
+
+    loopTextLines = async () => {
+        // TODO: IF you go the route of interactive console, this will probably need to become a promise
+        // NOTE: You _cannot_ await functions one by one in a .forEach loop
+        //       instead you need either a for..of loop or a Promise.all(array.map(line=> this.parseLine))
+        for (const line of this.textLines) {
+            await this.parseLine(line);
+        }
+    }
+
+    parseLine = async (line) => {
+        await this.appendNewParagraph();
+        const hasPrompt = line.split(' ')[0] === '#@#';
+        const hasLink = line.split(' ')[0].startsWith('$!link');
+
+        if (hasLink) {
+            await this.animateLinkText(line);
+            return
+        }
+
+        if (hasPrompt) {
+            await this.animatePromptText(line);
+            return
+        }
+
+        // continue to type out lines as given
+        this.hidePrompt()
+        await this.typeOut(line, this.currentParagraph);
+        this.showPrompt()
+    }
+
+    animatePromptText = async (text) => {
+        const restOfLine = text.split(' ').slice(1).join(' ');
+        this.stopCursorBlink();
+        await this.typeOutInCLI_Input(restOfLine)
+        this.cli_input.value = '';
+        this.currentParagraph.innerHTML = this.getCLIPrompt() + restOfLine;
+        this.startCursorBlink();
+    }
+
+    animateLinkText = async (line) => {
+        this.hidePrompt();
+        const linkHref = line.split(' ')[0].split('=').slice(1)
+        const restOfLine = line.split(' ').slice(1).join(' ');
+        const linkNode = this.buildLinkNode(linkHref);
+        this.currentParagraph.appendChild(linkNode);
+        await this.typeOut(restOfLine, linkNode);
+        this.showPrompt();
+    }
+
+    hidePrompt = () => {
+        if (this.prompt_wrapper) {
+            this.prompt_wrapper.style.display = 'none';
+        }
+    }
+
+    showPrompt = () => {
+        if (this.prompt_wrapper) {
+            this.prompt_wrapper.style.display = 'initial';
+        }
+    }
+
+    setRefs = () => {
+        this.cli_input = document.getElementById('cli_input');
+        this.prompt = document.getElementById('prompt');
+        this.prompt_wrapper = document.getElementById('prompt-wrapper');
+        this.display = document.getElementById('display');
+        this.content = document.getElementById('content');
+    }
+
+    getCLIPrompt = () => {
         return '<span class="cli-prompt">bryan.hoffmaster@HAL9000</span>:<span class="prompt-tilde">~</span><span class="prompt-dollar-sign">$</span>';
-    },
+    }
 
-    updateLastCharacter: ()=> {
-        const content = this.CLI_Display.content();
-        if(!content) console.error('Content is not defined in CLI_Display.updateLastCharacter');
+    buildLinkNode = (href) => {
+        var link = document.createElement('a');
+        link.setAttribute('href', href);
+        link.addEventListener('click', (e) => {
+            e.preventDefault();
+            e.stopImmediatePropagation();
+            e.stopPropagation();
+            window.open(e.target.href, '_blank');
+        });
+        return link;
+    }
 
-        const contentText = content.innerHTML;
+    appendNewParagraph = async () => {
+        var newParagraph = document.createElement('p');
+        this.content.appendChild(newParagraph);
+        this.currentParagraph = newParagraph;
+    }
 
-        if (contentText.substring(contentText.length - 1, contentText.length) == '|') {
-            // you want to append the content contentText
-            // should you pass the content to update with instead of checking here?
-        } else {
-            this.CLI_Display.console.innerHTML = '|'; // else write it
+    typeOut = (text, node=null) => {
+        return new Promise((resolve, reject) => {
+            var i = 0;
+            var timer = setInterval(() => {
+                if (i < text.length) {
+                    node.innerHTML += text.charAt(i);
+                    i++;
+                } else {
+                    clearInterval(timer);
+                    resolve();
+                }
+            }, 1);
+        });
+    }
+
+    typeOutInCLI_Input = (text) => {
+        return new Promise((resolve, reject) => {
+            var i = 0;
+            var timer = setInterval((text2) => {
+                if (i < text.length) {
+                    this.cli_input.value += text.charAt(i);
+                    i++;
+                } else {
+                    clearInterval(timer);
+                    resolve();
+                }
+            }, 1);
+        });
+
+    }
+
+    startCursorBlink = () => {
+        this.cursorBlinkRef = setInterval(function () {
+            var input = this.cli_input;
+            if (input.getAttribute('placeholder') == '|') {
+                input.setAttribute('placeholder', '');
+            } else {
+                input.setAttribute('placeholder', '|');
+            }
+        }, 250);
+    }
+
+    stopCursorBlink = () => {
+        this.cursorBlinkRef = clearInterval(this.cursorBlinkRef);
+    }
+
+    init = async () => {
+        const fileText = await getFileText(this.fileName);
+        this.text = fileText ?? '';
+        this.textLines = this.text.split('\n');
+        this.setRefs();
+
+        if (this.textLines.length > 0) {
+            this.loopTextLines();
         }
-            $('#console').html(
-                $('#console')
-                    .html()
-                    .substring(0, contentText.length - 1),
-            );
-    },
-
-    updateConsole: ()=> {
-        // TODO: just make the cursor blink by it's own through the input?
-        var console = document.getElementById('console');
-        console.innerHTML = getCLIPrompt() + ' |';
     }
 };
 
-
-var consoleInputTicker = setInterval(function () {
-    // TODO: hide the console input when writing out content!
-    CLI_Display.updateConsole();
-}, 300);
-
-CLI_Display.speed = 300;
-CLI_Display.file = 'BryanHoffmaster.txt';
-CLI_Display.updateConsole();
-CLI_Display.init();
-consoleInputTicker();
+// Wait til all content is loaded and then start the program off!
+window.onload = () => {
+    const display = new CLI_Display('BryanHoffmaster.txt');
+}
